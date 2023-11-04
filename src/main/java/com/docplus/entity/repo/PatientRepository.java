de25aa78c186f6ff937b5cedacd611707ef09b74
kapil <jmk.kapil@gmail.com>
2023-11-04 18:59:58 -0500

    added swagger fixed small issues

--- src/main/java/com/docplus/entity/repo/PatientRepository.java
+++ src/main/java/com/docplus/entity/repo/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.docplus.entity.repo;
 
 import com.docplus.entity.Patient;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface PatientRepository extends PagingAndSortingRepository<Patient, String> {
+@RepositoryRestResource(path = "patients")
+public interface PatientRepository extends MongoRepository<Patient, String> {
 }
--- src/main/java/com/docplus/entity/repo/VitalInfoRepository.java
+++ src/main/java/com/docplus/entity/repo/VitalInfoRepository.java
@@ -1,7 +1,10 @@
 package com.docplus.entity.repo;
 
 import com.docplus.entity.VitalInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface VitalInfoRepository extends PagingAndSortingRepository<VitalInfo, String> {
+@RepositoryRestResource(path = "vitalinfos")
+public interface VitalInfoRepository extends MongoRepository<VitalInfo, String> {
 }
